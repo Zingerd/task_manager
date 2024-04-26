eb1aa28e5d729dffdc50eb7cf5434cde3cceafcc
Zingerd <goroan-makcim@ukr.net>
2024-04-26 17:07:29 +0300

    remove comment

--- src/main/java/com/example/task_manager/conf/KafkaProducerConfig/MessageProducer.java
+++ src/main/java/com/example/task_manager/conf/KafkaProducerConfig/MessageProducer.java
@@ -24,7 +24,6 @@ public class MessageProducer {
             String message = objectMapper.writeValueAsString(task);
             kafkaTemplate.send(topic, message);
         } catch (JsonProcessingException e) {
-            // Обробити помилку серіалізації
             e.printStackTrace();
         }
     }
